597c9b51606767c0c0ea7ed69be7dbf7d377a9a0
Dwight Goins <dngoins@hotmail.com>
2015-07-19 15:59:29 -0600

    added face basic project and source

---
+++ FaceBasics-D2D/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FaceBasics.rc
+//
+#define IDI_APP                         100
+#define IDD_APP                         100
+#define IDR_MENU1                       101
+#define IDC_VIDEOVIEW                   1000
+#define IDC_STATUS                      1001
+#define IDC_BUTTON_SCREENSHOT           1002
+#define ID_OPTIONS_SHOWFACEBOX          32771
+#define ID_OPTIONS_SHOWFACEPOINTS       32772
+#define ID_OPTIONS_SHOWHDFACEPOINTS     32773
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
